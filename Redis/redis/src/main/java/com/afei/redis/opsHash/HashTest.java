a9b66b1ea59b01b485c57f9b8e89eb59c1b95f1a
王若飞 <wangruofei@eccom.com.cn>
2021-12-14 17:31:27 +0800

    1214 -1

--- Redis/redis/src/main/java/com/afei/redis/opsHash/HashTest.java
+++ Redis/redis/src/main/java/com/afei/redis/opsHash/HashTest.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-public class HashTest {
+public class HashTest {                     // <H, HK, HV>
 
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
@@ -22,10 +22,23 @@ public class HashTest {
         map1.put("k2", 2);
         map1.put("k3", 3);
         redisTemplate.opsForHash().putAll("map1", map1);
+        redisTemplate.opsForHash().delete("map1", "k1");    // 删除H中的 HK=“k1” , HK可以是多个值
+        redisTemplate.opsForHash().put("map1", "k1", "v1");  // 往H中放入, <HK, HV>
         Map<Object, Object> map1Redis = redisTemplate.opsForHash().entries("map1");
         for (Map.Entry<Object, Object> mapFromRedis : map1Redis.entrySet()) {
-            System.out.println((String) mapFromRedis.getKey() + ": " + (Integer) mapFromRedis.getValue());
+            System.out.println((String) mapFromRedis.getKey() + ": " + String.valueOf(mapFromRedis.getValue()));
         }
+
+        System.out.println(redisTemplate.opsForHash().get("map1", "k2"));   // 根据HK获得H中的HV
+        redisTemplate.opsForHash().put("map1", "k4", 3);  // 往H中放入, <HK, HV>
+
+        System.out.println(redisTemplate.opsForHash().increment("map1", "k4", 2));
+
+        System.out.println(redisTemplate.opsForHash().increment("map1", "k4", -3));
+
+        System.out.println(redisTemplate.opsForHash().hasKey("map1", "k5"));
+
+        System.out.println(redisTemplate.opsForHash().lengthOfValue("map1", "k1"));
 
         return "success!";
     }
